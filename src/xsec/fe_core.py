8f5702d07f3f86790260b48ba522f9e262bc12e3
xmdi <m@xmdi.us>
2026-09-16 22:20:26 -0400

    lot of work, should resolve #5 and various bugs

--- src/xsec/fe_core.py
+++ src/xsec/fe_core.py
@@ -25,29 +25,16 @@ def evaluate_gauss_point(element_type, nodes, gp):
         dN2_de = 0.25 * (1 - gp[1])
         dN2_dn = 0.25 * (-1 - gp[0])
         N3 = 0.25 * (1 + gp[0]) * (1 + gp[1])
-        dN3_de = 0.25 * (1 - gp[1])
-        dN3_dn = 0.25 * (1 - gp[0])
+        dN3_de = 0.25 * (1 + gp[1])
+        dN3_dn = 0.25 * (1 + gp[0])
         N4 = 0.25 * (1 - gp[0]) * (1 + gp[1])
         dN4_de = 0.25 * (-1 - gp[1])
         dN4_dn = 0.25 * (1 - gp[0])
-        N = np.zeros((3, 12), dtype = np.float64)
+        N = np.zeros((3, 12))
         N[0, :] = [N1, 0, 0, N2, 0, 0, N3, 0, 0, N4, 0, 0]
         N[1, :] = [0, N1, 0, 0, N2, 0, 0, N3, 0, 0, N4, 0]
         N[2, :] = [0, 0, N1, 0, 0, N2, 0, 0, N3, 0, 0, N4]
-        # dN = [dN1_de 0 0 DN2_de 0 0 DN3_de 0 0 DN4_de 0 0 
-        #       0 dN1_dn 0 0 DN2_dn 0 0 DN3_dn 0 0 DN4_dn 0
-        #       dN1_dn dN1_de 0 DN2_dn DN2_de 0 DN3_dn DN3_de 0 DN4_dn DN4_de 0
-        #       0 0 DN1_de 0 0 DN2_de 0 0 DN3_de 0 0 DN4_de
-        #       0 0 DN1_dn 0 0 DN2_dn 0 0 DN3_dn 0 0 DN4_dn
-        #       0 0 0 0 0 0 0 0 0 0 0 0]
-        # (dN is usually called the B matrix)
-        dN = np.zeros((6, 12), dtype = np.float64)
-        dN[0, :] = [dN1_de, 0, 0, DN2_de, 0, 0, DN3_de, 0, 0, DN4_de, 0, 0]
-        dN[1, :] = [0, dN1_dn, 0, 0, DN2_dn, 0, 0, DN3_dn, 0, 0, DN4_dn, 0]
-        dN[2, :] = [dN1_dn, dN1_de, 0, DN2_dn, DN2_de, 0, DN3_dn, DN3_de, 0, DN4_dn, DN4_de, 0]
-        dN[3, :] = [0, 0, dN1_de, 0, 0, DN2_de, 0, 0, DN3_de, 0, 0, DN4_de]
-        dN[4, :] = [0, 0, dN1_dn, 0, 0, DN2_dn, 0, 0, DN3_dn, 0, 0, DN4_dn]
-        dN[5, :] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
         # J0 = [dN1/de dN2/de dN3/de dN4/de
         #       dN1/dn dN2/dn dN3/dn dN4/dn] (2x4)
         # nodes = [x1 y1
@@ -55,26 +42,98 @@ def evaluate_gauss_point(element_type, nodes, gp):
         #          x3 y3
         #          x4 y4] (4x2)
         # J = J0 * nodes (2x2)
-        J0 = np.zeros((2, 4), dtype = np.float64)
-        J0[0, :] = [dN1_de, dN2_de, DN3_de DN4_de]
-        J0[1, :] = [dN1_dn, dN2_dn, DN3_dn DN4_dn]
+        J0 = np.zeros((2, 4))
+        J0[0, :] = [dN1_de, dN2_de, dN3_de, dN4_de]
+        J0[1, :] = [dN1_dn, dN2_dn, dN3_dn, dN4_dn]
         J = J0 @ nodes
         # det(J) = J[0, 0] * J[1, 1] - J[1, 0] * J[0, 1]
         # inv(J) = [J[1, 1], -J[0, 1]; -J[1, 0], J[0, 0]] / det(J)
         det_J = J[0, 0] * J[1, 1] - J[1, 0] * J[0, 1]
-        J_inv = np.zeros((2, 2), dtype = np.float64)
+        J_inv = np.zeros((2, 2))
         J_inv[0, :] = [J[1, 1], -J[0, 1]] / det_J
         J_inv[1, :] = [-J[1, 0], J[0, 0]] / det_J
-   else:
-        raise(f"Unsupported element_type ({element_type}) in evaluate_gauss_point().")
-    return N, dN, J, det_J, J_inv
+
+        # dN = [dN1_de 0 0 DN2_de 0 0 DN3_de 0 0 DN4_de 0 0 
+        #       0 dN1_dn 0 0 DN2_dn 0 0 DN3_dn 0 0 DN4_dn 0
+        #       dN1_dn dN1_de 0 DN2_dn DN2_de 0 DN3_dn DN3_de 0 DN4_dn DN4_de 0
+        #       0 0 DN1_de 0 0 DN2_de 0 0 DN3_de 0 0 DN4_de
+        #       0 0 DN1_dn 0 0 DN2_dn 0 0 DN3_dn 0 0 DN4_dn
+        #       0 0 0 0 0 0 0 0 0 0 0 0]
+        # (dN is usually called the B matrix)
+        # chain rule:
+        dN_dx = np.zeros((4, 2))
+        dN_dx[0, :] = J_inv @ np.array([dN1_de, dN1_dn])
+        dN_dx[1, :] = J_inv @ np.array([dN2_de, dN2_dn])
+        dN_dx[2, :] = J_inv @ np.array([dN3_de, dN3_dn])
+        dN_dx[3, :] = J_inv @ np.array([dN4_de, dN4_dn])
+        
+        dN = np.zeros((6, 12))
+        dN[0, :] = [dN_dx[0,0], 0, 0, dN_dx[1,0], 0, 0, dN_dx[2,0], 0, 0, dN_dx[3,0], 0, 0]
+        dN[1, :] = [0, dN_dx[0,1], 0, 0, dN_dx[1,1], 0, 0, dN_dx[2,1], 0, 0, dN_dx[3,1], 0]
+        dN[2, :] = [dN_dx[0,1], dN_dx[0,0], 0, dN_dx[1,1], dN_dx[1,0], 0, dN_dx[2,1], dN_dx[2,0], 0, dN_dx[3,1], dN_dx[3,0], 0]
+        dN[3, :] = [0, 0, dN_dx[0,0], 0, 0, dN_dx[1,0], 0, 0, dN_dx[2,0], 0, 0, dN_dx[3,0]]
+        dN[4, :] = [0, 0, dN_dx[0,1], 0, 0, dN_dx[1,1], 0, 0, dN_dx[2,1], 0, 0, dN_dx[3,1]]
+        dN[5, :] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    else:
+        raise ValueError(f"Unsupported element_type ({element_type}) in evaluate_gauss_point().")
+    return N, dN, det_J
 
 def constitutive_matrix_isotropic(E, v):
     """Generates isotropic elastic constitutive matrix. All inputs should be np.float64"""
     G = E / (2 * (1 + v))
     Q1 = (E * (1 - v)) / ((1 + v) * (1 - 2 * v))
     Q2 = (E * v) / ((1 + v) * (1 - 2 * v))
-    D = np.zeros((6, 6), dtype = np.float64)
-    D[0:2,0:2] = [[Q1, Q2, Q2], [Q2, Q1, Q2], [Q2, Q2, Q1]]
-    D[3:5,3:5] = [[G, 0, 0], [0, G, 0], [0, 0, G]]
+    D = np.zeros((6, 6))
+    normal_idx = [0, 1, 5]   # eps11, eps22, eps33
+    for i in normal_idx:
+        for j in normal_idx:
+            D[i, j] = Q1 if i == j else Q2
+    D[2, 2] = G   # gamma12
+    D[3, 3] = G   # gamma13
+    D[4, 4] = G   # gamma23
     return D
+
+def elemental_matrices(element_type,nodes):
+    """Generates elemental matrices Mi, Ci, Ei, Li & Ri."""
+    S = np.array([
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0]])
+    E=10.0e6
+    v=0.3
+    D = constitutive_matrix_isotropic(E, v) # TODO: need to generalize this, probably compute outside of this function and reused
+    DS = D @ S
+    SDS = S.T @ DS
+    if element_type == 3: # bilinear quad
+        gp0 = 1.0 / np.sqrt(3.0)
+        gps = np.array([
+            [-gp0, -gp0],
+            [gp0,  -gp0],
+            [gp0,   gp0],
+            [-gp0,  gp0]])
+        Mi = np.zeros((12,12))
+        Ci = np.zeros((12,12))
+        Ei = np.zeros((12,12))
+        Li = np.zeros((12,6))
+        Ri = np.zeros((12,6))
+        for gp in gps:
+            N, dN, det_J = evaluate_gauss_point(element_type, nodes, gp)
+            Nvals = N[0, 0::3] # grab [N1,N2,N3,N4]
+            x1_gp = Nvals @ nodes[:, 0]
+            x2_gp = Nvals @ nodes[:, 1]
+            Z = np.array([
+                [1.0, 0.0, 0.0, 0.0, 0.0, -x2_gp],
+                [0.0, 1.0, 0.0, 0.0, 0.0, x1_gp],
+                [0.0, 0.0, 1.0, x2_gp, -x1_gp, 0.0]])
+            Mi += N.T @ SDS @ N * det_J # * w but gauss point weight = 1.0
+            Ci += dN.T @ DS @ N * det_J # * w but gauss point weight = 1.0
+            Ei += dN.T @ D @ dN * det_J # * w but gauss point weight = 1.0
+            Li += N.T @ SDS @ Z * det_J # * w but gauss point weight = 1.0
+            Ri += dN.T @ DS @ Z * det_J # * w but gauss point weight = 1.0
+    else:
+        raise ValueError(f"Unsupported element_type ({element_type}) in elemental_matrices().")
+
+    return Mi, Ci, Ei, Li, Ri
